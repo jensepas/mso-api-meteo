63f0e6a4dd269dc5c13c219a2166a45f2a9988bc
gaga <jensepas@hotmail.com>
2020-04-19 18:16:08 +0200

    upgrade symfony 3.4

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
  * @location /index.php
  */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 define('DIR_ROOT', __DIR__);
 
