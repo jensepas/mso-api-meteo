bb0ac435948af3ec8a1ae642161d6ce2793e6573
gaga <jensepas@hotmail.com>
2020-04-21 19:00:08 +0200

    upgrade symfony 5

--- lib/params.php
+++ lib/params.php
@@ -3,7 +3,7 @@
 define('ES_ADRESS', 'localhost:9200');
 define('ES_INDEX', 'air_measurements');
 
-define('BDD_ADRESS', '192.168.0.30');
+define('BDD_ADRESS', 'localhost');
 define('BDD_BASE', 'meteo');
-define('BDD_LOGIN', 'meteo');
-define('BDD_PASSWORD', 'klikeul');
+define('BDD_LOGIN', 'root');
+define('BDD_PASSWORD', 'Klikeul!0607');
