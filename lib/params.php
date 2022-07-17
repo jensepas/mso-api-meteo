658350a305e77d2201965ca871c8678a8b0dc0d5
jensepas <80039639+jensepas@users.noreply.github.com>
2022-07-17 12:02:09 +0200

    Mise à jour

--- lib/params.php
+++ lib/params.php
@@ -4,6 +4,6 @@ define('ES_ADRESS', 'localhost:9200');
 define('ES_INDEX', 'air_measurements');
 
 define('BDD_ADRESS', 'localhost');
-define('BDD_BASE', '');
-define('BDD_LOGIN', '');
-define('BDD_PASSWORD', '');
+define('BDD_BASE', 'meteo');
+define('BDD_LOGIN', 'root');
+define('BDD_PASSWORD', 'Klikeul!0607');
