dce42c5bbc7a8e2b775392bacc4799efa76a791f
jensepas <80039639+jensepas@users.noreply.github.com>
2024-10-11 11:08:36 +0200

    Laravel V11 Upgrade

--- app/Models/Meteo.php
+++ app/Models/Meteo.php
@@ -4,21 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use MatanYadaev\EloquentSpatial\Objects\Point;
-use MatanYadaev\EloquentSpatial\SpatialBuilder;
+use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 
-/**
- * @method static SpatialBuilder query()
- */
 class Meteo extends Model
 {
+    use HasSpatial;
+
     protected $fillable = ['title', 'description', 'coordinates'];
 
     protected $casts = [
         'coordinates' => Point::class,
     ];
-
-    public function newEloquentBuilder($query): SpatialBuilder
-    {
-        return new SpatialBuilder($query);
-    }
 }
